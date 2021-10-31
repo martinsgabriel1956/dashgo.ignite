68375f9cf337f06113d081b1b78e0d4422d88b59
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-10-31 09:42:56 -0300

    feat: signalizing ActiveLink

--- src/components/SideBar/NavLink/index.tsx
+++ src/components/SideBar/NavLink/index.tsx
@@ -5,7 +5,8 @@ import {
   Text,
   LinkProps as ChakraLinkProps,
 } from "@chakra-ui/react";
-import Link from "next/link";
+
+import { ActiveLink } from "../../ActiveLink";
 
 interface NavLinkProps extends ChakraLinkProps {
   children: string;
@@ -15,13 +16,13 @@ interface NavLinkProps extends ChakraLinkProps {
 
 export function NavLink({ children, icon, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" align="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text fontWeight="medium" ml="4">
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
