b11d1b71e07e0b67ccfe632e98fee720a3710dd7
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-10-30 22:15:10 -0300

    feat: header responsive

--- src/components/Header/Profile/index.tsx
+++ src/components/Header/Profile/index.tsx
@@ -1,21 +1,26 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
-  return (
-    <Flex
-    align="center"
-  >
-    <Box mr="4" textAlign="right">
-      <Text>Gabriel Martins</Text>
-      <Text
-        color="gray.300"
-        fontSize="small"
-      >martinsgabriel1956@gmail.com</Text>
-    </Box>
-
-    <Avatar size="md" name="Gabriel Martins" src="https://github.com/martinsgabriel1956.png" />
-  </Flex>
-  );
+interface ProfileProps {
+  showProfileData?: boolean;
 }
 
+export function Profile({ showProfileData = true }: ProfileProps) {
+  return (
+    <Flex align="center">
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gabriel Martins</Text>
+          <Text color="gray.300" fontSize="small">
+            martinsgabriel1956@gmail.com
+          </Text>
+        </Box>
+      )}
 
+      <Avatar
+        size="md"
+        name="Gabriel Martins"
+        src="https://github.com/martinsgabriel1956.png"
+      />
+    </Flex>
+  );
+}
