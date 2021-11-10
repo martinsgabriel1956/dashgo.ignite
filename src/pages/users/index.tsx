2229386c3fca724b5f1c47ab646b2170bd417dea
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-11-09 23:00:16 -0300

    feat: creating list hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,14 +18,13 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from "react-query";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
-import { api } from "../../services/api";
+
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
@@ -33,26 +32,7 @@ export default function UserList() {
     lg: true,
   });
 
-  const { data, isLoading, isFetching , error } = useQuery("users", async () => {
-    const { data } = await api.get("users");
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        }),
-      }
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5 // 5 seconds past
-  });
+  const { data, isLoading, isFetching , error } = useUsers();
 
   return (
     <Box>
